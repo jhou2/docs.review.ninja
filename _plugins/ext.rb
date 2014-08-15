8dd863b5024f1f6267dbc71e79b3cf395364b1a7
flower-pot <flower-pot@flowerpot.io>
2014-08-15 16:55:21 -0700

    initial commit

---
+++ _plugins/ext.rb
@@ -0,0 +1,8 @@
+require 'compass'
+require 'sass'
+require 'uglifier'
+
+require 'jekyll-assets'
+require 'jekyll-assets/compass'
+require 'jekyll-assets/bootstrap'
+require 'jekyll-assets/font-awesome'
